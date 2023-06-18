39d2c40cfb63195bd5cfa1d3f34939b32db21196
Pompa123 <52543003+pompa-123@users.noreply.github.com>
2023-06-18 14:52:28 +0300

    mangareader bölüm indirici

---
+++ server/query.php
@@ -0,0 +1,38 @@
+<?php
+
+$id = $_GET["id"];
+$auth = $_GET["auth"];
+if ($id == null) {
+exit();}
+if (empty($id)) {
+    exit();
+   
+}
+if (empty($auth)) {
+ exit();
+}
+
+if ($auth != "AUTH") {
+   exit();
+}
+$host = "localhost";
+$db = "dbname";
+$user = "user";
+$pass = "password";
+
+$conn = new PDO("mysql:host=$host;dbname=$db", "$user", "$pass");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sorgu = $conn->prepare("SELECT * FROM wp_posts WHERE ID='$id'");
+
+$sorgu->execute();
+if ($sorgu->rowCount() <= 0) {
+    echo'<h1 class="entry-title">notfound</h1>';
+    exit();
+} else {
+$sonuc = $sorgu->fetchAll();
+echo'<h1 class="entry-title">'.$sonuc[0]["post_name"].'</h1>';
+$json = $sonuc[0]["post_content"];
+$jsonString = str_replace(utf8_encode("<img"),"<img id='pompala'",$json);
+
+print_r ($jsonString);
+}
